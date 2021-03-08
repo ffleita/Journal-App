fe90e6aa1e794067ccd593873448b8845bb1c19c
Facundo Fleitas <facundoezequielfleitas96@gmail.com>
2021-03-08 20:28:59 -0300

    first commit

---
+++ src/components/journal/JournalEntry.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export const JournalEntry = () => {
+    return (
+        <div className="journal__entry pointer">
+            <div
+                className="journal__entry-picture"
+                style={{
+                    backgroundSize: 'cover',
+                    backgroundImage: 'url(https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png)'
+                }}
+            ></div>
+
+            <div className="journal__entry-body">
+                <p className="journal__entry-title">
+                    Un nuevo día
+                </p>
+
+                <p className="journal__entry-content">
+                    Lorem ipsum
+                </p>
+            </div>
+
+            <div className="journal__entry-date-box">
+                <span>Monday</span>
+                <h4>28</h4>
+            </div>
+        </div>
+    )
+}
