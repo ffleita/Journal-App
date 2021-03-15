d7d8c346c639928d9504fb0c8d84d3f5e9dbd4d0
ffleita <72566961+ffleita@users.noreply.github.com>
2021-03-15 13:42:29 -0300

    Corrijo LoginScreen.js (#2)

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -13,7 +13,7 @@ export const LoginScreen = () => {
                     type="text"
                     placeholder="Email"
                     name="email"
-                    autoComplete="off"asd
+                    autoComplete="off"
                 />
 
                 <input 
